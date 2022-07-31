1dcc91bba4b2c0cf52870f78a4c9fa22ef915371
TonyPepe <41405045+TonyPepeBear@users.noreply.github.com>
2022-07-31 03:49:05 +0000

    Update

--- static/g-analytics.js
+++ static/g-analytics.js
@@ -1,5 +1,7 @@
-<script>
-  window.dataLayer = window.dataLayer || []; function gtag()
-  {dataLayer.push(arguments)}
-  gtag('js', new Date()); gtag('config', 'G-P8BKJWQW7W');
-</script>;
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "G-P8BKJWQW7W");
